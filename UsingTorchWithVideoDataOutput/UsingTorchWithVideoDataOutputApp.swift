cecbea3b2ab121f18c51914c14384b75972b3073
Yoshiyuki Koyanagi <moutend@gmail.com>
2024-07-21 21:16:20 +0900

    Add files

---
+++ UsingTorchWithVideoDataOutput/UsingTorchWithVideoDataOutputApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsingTorchWithVideoDataOutputApp.swift
+//  UsingTorchWithVideoDataOutput
+//
+//  Created by Yoshiyuki Koyanagi on 2024/07/21.
+//
+
+import SwiftUI
+
+@main
+struct UsingTorchWithVideoDataOutputApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
