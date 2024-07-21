2c0f28cc4a73f63d8e416e9d2fed0bb3065dadcd
Yoshiyuki Koyanagi <moutend@gmail.com>
2024-07-21 21:36:40 +0900

    Update content view

--- UsingTorchWithVideoDataOutput/ContentView.swift
+++ UsingTorchWithVideoDataOutput/ContentView.swift
@@ -1,26 +1,45 @@
-//
-//  ContentView.swift
-//  UsingTorchWithVideoDataOutput
-//
-//  Created by Yoshiyuki Koyanagi on 2024/07/21.
-//
-
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+  private let imageClassifier = ImageClassifier()
+  private let cameraManager = CameraManager()
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+  @State private var predictions: [ImageClassifier.Prediction] = []
+  @State private var torch = false
+
+  var body: some View {
+    VStack {
+      PreviewView(captureSession: self.cameraManager.captureSession)
+        .frame(width: UIScreen.main.bounds.size.width)
+      Button(action: {
+        self.torch = self.cameraManager.toggleTorch()
+      }) {
+        Text(self.torch ? "Disable Torch" : "Enable Torch")
+          .padding()
+          .foregroundColor(.white)
+          .background(.indigo)
+      }
+      .padding()
+      Text("Predictions")
+        .font(.title)
+        .bold()
+      if let prediction = self.predictions.first {
+        Text(
+          "\(prediction.classification) - \(prediction.confidence * 100.0, specifier: "%.0f")%"
+        )
+        .padding()
+      }
     }
+    .onAppear {
+      self.cameraManager.delegate = self.imageClassifier
+      self.cameraManager.start()
+    }
+    .onDisappear {
+      self.cameraManager.delegate = nil
+      self.cameraManager.stop()
+    }
+    .onReceive(self.imageClassifier.result) { predictions in
+      self.predictions = [ImageClassifier.Prediction](predictions.prefix(3))
+    }
+  }
 }
--- UsingTorchWithVideoDataOutput/UsingTorchWithVideoDataOutputApp.swift
+++ UsingTorchWithVideoDataOutput/UsingTorchWithVideoDataOutputApp.swift
@@ -1,17 +1,10 @@
-//
-//  UsingTorchWithVideoDataOutputApp.swift
-//  UsingTorchWithVideoDataOutput
-//
-//  Created by Yoshiyuki Koyanagi on 2024/07/21.
-//
-
 import SwiftUI
 
 @main
 struct UsingTorchWithVideoDataOutputApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
